de89436a242c68220fe4dd529c5fd98785e2e871
Avraneel Pal <avraneelpal22@gmail.com>
2026-04-04 23:19:30 +0530

    Fix rehash and remove algorithms

--- HashMap.js
+++ HashMap.js
@@ -1,12 +1,14 @@
+import { LinkedList } from "./LinkedList.js";
+
 export class HashMap {
   constructor(loadFactor, capacity) {
     this.loadFactor = loadFactor ?? 0.75;
     this.capacity = capacity ?? 16;
-    this.data = Array(this.capacity);
+    this.buckets = Array(this.capacity);
   }
 
   checkBounds(index) {
-    if (index < 0 || index >= this.data.length) {
+    if (index < 0 || index >= this.buckets.length) {
       throw new Error("Trying to access index out of bounds");
     }
   }
@@ -14,28 +16,54 @@ export class HashMap {
   hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
-
     for (let i = 0; i < key.length; i++) {
       hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
-      console.log(hashCode);
     }
     return hashCode;
   }
 
   rehash() {
-    const backup = JSON.parse(JSON.stringify(this.data));
+    const backupBuckets = JSON.parse(JSON.stringify(this.buckets));
     this.capacity *= 2;
-    this.data = Array(this.capacity);
-    backup.forEach((item) => {
-      const index = this.hash(item);
-      this.data[index] = item;
+    this.buckets = Array(this.capacity);
+    backupBuckets.forEach((bucket) => {
+      // undefined isnt valid JSON value. If undefined is occured during array, it
+      // is converted to null
+      if (bucket !== null) {
+        let pointer = bucket.headNode;
+        while (pointer !== null) {
+          //const newIndex = this.hash(pointer.value.key);
+          this.set(pointer.value.key, pointer.value.value);
+          pointer = pointer.nextNode;
+        }
+      }
+      //const index = this.hash(item);
+      //this.buckets[index] = item;
     });
   }
 
   set(key, value) {
     const index = this.hash(key);
     this.checkBounds(index);
-    this.data[index] = { key: key, value: value };
+
+    if (this.buckets[index] === undefined) {
+      // bucket is empty, insert a linked list
+      const ll = new LinkedList({ key: key, value: value });
+      this.buckets[index] = ll;
+    } else {
+      // bucket already has a linked list
+      let pointer = this.buckets[index].headNode;
+      while (pointer !== null) {
+        if (pointer.value.key === key) {
+          // if key exists, then overwrite
+          pointer.value.value = value;
+          return;
+        }
+        pointer = pointer.nextNode;
+      }
+      // at this point, key does not exist. So append
+      this.buckets[index].append({ key: key, value: value });
+    }
     if (this.length() > this.capacity * this.loadFactor) {
       this.rehash();
     }
@@ -43,66 +71,112 @@ export class HashMap {
 
   get(key) {
     const index = this.hash(key);
-    if (data[index]) return data[index].value;
-    else return null;
+    this.checkBounds(index);
+    if (this.buckets[index] !== undefined) {
+      // If bucket is non-empty then search through the list
+      let pointer = this.buckets[index].headNode;
+      while (pointer !== null) {
+        // Iterate through the list
+        if (key === pointer.value.key) {
+          return pointer.value.value;
+        }
+        pointer = pointer.nextNode;
+      }
+    } else {
+      return null;
+    }
   }
 
   has(key) {
     const index = this.hash(key);
-    const val = this.data[index].value;
-    return val == undefined ? false : true;
+    this.checkBounds(index);
+    // jump to the bucket index
+    const chainHead = this.buckets[index];
+    return chainHead.contains(key);
   }
 
   remove(key) {
     const index = this.hash(key);
-    const val = this.data[index].value;
-    if (val != undefined) {
-      // key found
-      this.data[index] = undefined;
+    this.checkBounds(index);
+    const chainHead = this.buckets[index].headNode;
+    if (chainHead === null) {
+      // already empty, nothing to do
+      return false;
+    } else if (chainHead.nextNode === null) {
+      // if only 1 element exists in bucket
+      this.buckets[index] = undefined;
       return true;
     } else {
-      return false;
+      // if bucket has > 1 size linked list
+      let pointer = chainHead;
+      if (pointer.value.key === key) {
+        // if head is the node to be deleted
+        this.buckets[index].headNode = this.buckets[index].headNode.nextNode;
+        return true;
+      }
+      while (pointer.nextNode.value.key !== key) {
+        // iterate to the previous node before the node to be deleted
+        pointer = pointer.nextNode;
+      }
+      // now node is at previous node of the node to be deleted
+      pointer.nextNode = pointer.nextNode.nextNode;
+      return true;
     }
   }
 
   length() {
     let count = 0;
-    for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i] != undefined) {
-        count++;
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (this.buckets[i] != undefined) {
+        count += this.buckets[i].size();
       }
     }
     return count;
   }
 
   clear() {
-    for (let i = 0; i < this.data.capacity; i++) {
-      data[i] = undefined;
+    for (let i = 0; i < this.buckets.capacity; i++) {
+      buckets[i] = undefined;
     }
   }
 
   keys() {
     const arr = [];
-    this.data.forEach((element) => {
-      if (element != undefined) arr.push(element.key);
+    this.buckets.forEach((bucket) => {
+      if (bucket != undefined) {
+        let pointer = bucket.headNode;
+        while (pointer != null) {
+          arr.push(pointer.value.key);
+          pointer = pointer.nextNode;
+        }
+      }
     });
     return arr;
   }
 
   values() {
     const arr = [];
-    this.data.forEach((element) => {
-      if (element != undefined) arr.push(element.value);
+    this.buckets.forEach((bucket) => {
+      if (bucket != undefined) {
+        let pointer = bucket.headNode;
+        while (pointer != null) {
+          arr.push(pointer.value.value);
+          pointer = pointer.nextNode;
+        }
+      }
     });
     return arr;
   }
 
   entries() {
     const arr = [];
-    this.data.forEach((element) => {
-      if (element != undefined) {
-        const item = [element.key, element.value];
-        arr.push(item);
+    this.buckets.forEach((bucket) => {
+      if (bucket != undefined) {
+        let pointer = bucket.headNode;
+        while (pointer != null) {
+          arr.push([pointer.value.key, pointer.value.value]);
+          pointer = pointer.nextNode;
+        }
       }
     });
     return arr;
--- HashMapTest.js
+++ HashMapTest.js
@@ -1,23 +1,42 @@
 import { HashMap } from "./HashMap.js";
 
 const test = new HashMap();
-console.log(test.hash("apple"));
 test.set("apple", "red");
-// test.set("banana", "yellow");
-// test.set("carrot", "orange");
-// test.set("dog", "brown");
-// test.set("elephant", "gray");
-// test.set("frog", "green");
-// test.set("grape", "purple");
-// test.set("hat", "black");
-// test.set("ice cream", "white");
-// test.set("jacket", "blue");
-// test.set("kite", "pink");
-// test.set("lion", "golden");
-// console.log(test.length());
-// console.log(test.values());
-// console.log(test.entries());
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
 
+console.log(test.length());
+console.log(test.keys());
+console.log(test.values());
+console.log(test.entries());
+
+test.set("elephant", "circus");
+console.log(`Load now: ${test.length() / test.capacity}`);
+console.log(test.get("elephant"));
+console.log(test.length());
+
+test.set("moon", "silver");
+console.log(test.length());
+console.log(test.capacity);
+console.log(`Load now: ${test.length() / test.capacity}`);
+
+test.set("kite", "new year");
+test.set("dog", "pet");
+test.set("lion", "natgeo");
+console.log(test.get("kite"));
+console.log(test.get("dog"));
+console.log(test.get("lion"));
+console.log(test.length());
+console.log(test.remove("lion"));
 // test.keys().forEach((el) => {
 //   console.log(`${el} ${test.hash(el)}`);
 // });
