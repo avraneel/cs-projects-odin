2654128b0767fecf760c435f946586ad9926025c
Avraneel Pal <avraneelpal22@gmail.com>
2026-04-12 13:49:12 +0530

    Add BFS

--- KnightsTravalis.js
+++ KnightsTravalis.js
@@ -5,7 +5,7 @@ class Vertex {
   }
 }
 
-function toFlatIndex(x, y) {
+function flatIndex(x, y) {
   return x - 1 + 8 * (y - 1);
 }
 
@@ -39,4 +39,39 @@ function genAdjList() {
   return board;
 }
 
-console.log(genAdjList());
+/**
+ * @param s [x,y] vertex
+ * @param graph Adjacency List
+ */
+function bfs(source, graph) {
+  const visited = new Array(64).fill(Infinity);
+  const res = [];
+  const q = [];
+
+  // Initialize root node
+  q.push(source);
+  visited[flatIndex(source[0], source[1])] = 1;
+
+  while (q.length > 0) {
+    let node = q.shift();
+    res.push(node);
+
+    let ind = flatIndex(node[0], node[1]); // index corresponding to position (x,y) in adjacency list
+    for (let j = 0; j < graph[ind].length; j++) {
+      let neighbour = graph[ind][j];
+      let neighbourIndex = flatIndex(neighbour[0], neighbour[1]);
+      if (visited[neighbourIndex] == Infinity) {
+        q.push(neighbour);
+        visited[neighbourIndex] = 1;
+      }
+    }
+  }
+
+  return res;
+}
+
+const board = genAdjList();
+console.log(board[flatIndex(3, 3)]);
+const path = bfs([3, 3], board);
+console.log(path);
+console.log(path.length);
